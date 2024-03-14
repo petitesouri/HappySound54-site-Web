a4483bd3a48b6815465c272f297a7c6a670dc1b5
Virginie RUDOWSKI <dipie89@hotmail.fr>
2024-03-14 12:09:55 +0100

    Add public folder for deployment

---
+++ public/component---src-pages-mariage-js-b6c41527546f3772d4ce.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[341],{9662:function(e,t,a){a.r(t),a.d(t,{Head:function(){return u}});var l=a(7294),n=a(1883),r=a(8496),c=a(9357);const u=()=>l.createElement(c.Z,{title:"Page mariage"});t.default=()=>l.createElement(r.Z,null,l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page Mariage"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-mariage-js-b6c41527546f3772d4ce.js.map
