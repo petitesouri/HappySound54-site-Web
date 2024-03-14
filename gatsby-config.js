deea593b409ab473df715fcc58eb76262875d2d5
Virginie RUDOWSKI <dipie89@hotmail.fr>
2024-03-14 16:02:02 +0100

    correction untracked files

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         theme_color: `#3A353A`,
         display: `minimal-ui`,
-        icon: `src/images/Logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/Logo-HS54.png`, // This path is relative to the root of the site.
       },
     },
   ],
