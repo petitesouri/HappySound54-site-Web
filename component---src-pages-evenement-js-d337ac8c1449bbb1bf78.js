c10dd769fe5895d7c934d25d1a1732a0dc25a387
Virginie RUDOWSKI <dipie89@hotmail.fr>
2024-03-14 16:47:07 +0100

    Updates

---
+++ component---src-pages-evenement-js-d337ac8c1449bbb1bf78.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[686],{4464:function(e,t,a){a.r(t),a.d(t,{Head:function(){return u}});var l=a(7294),n=a(1883),r=a(6010),c=a(9357);const u=()=>l.createElement(c.Z,{title:"Page four"});t.default=()=>l.createElement(r.Z,null,l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-evenement-js-d337ac8c1449bbb1bf78.js.map
